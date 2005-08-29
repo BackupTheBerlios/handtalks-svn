5fedc0c969b1bbbcaa18a322200f28ee2f6cec71
acdcp <acdcp@922b9c94-87f7-0310-a3a0-d3017a4e10ae>
2005-08-29 23:51:14 +0000

    combo muda label

--- tronco/pc/janela.py
+++ tronco/pc/janela.py
@@ -1,7 +1,6 @@
-#Boa:Frame:frJanela
 #-----------------------------------------------------------------------------
 # Name:        janela.py
-# Purpose:     
+# Purpose:     Desenhar janela TODO: Separar logica de negocio)
 #
 # Author:      Andre Pinto
 #
@@ -10,6 +9,7 @@
 # Copyright:   (c) 2005
 # Licence:     GPL
 #-----------------------------------------------------------------------------
+#Boa:Frame:frJanela
 
 import wx
 
@@ -17,11 +17,11 @@ def create(parent):
     return frJanela(parent)
 
 [wxID_FRJANELA, wxID_FRJANELABTSAIR, wxID_FRJANELABTTOCAR, 
- wxID_FRJANELACHLETRAS, wxID_FRJANELASBESTADO, wxID_FRJANELASTLETRA, 
+ wxID_FRJANELACHLETRAS, wxID_FRJANELASTBESTADO, wxID_FRJANELASTLETRA, 
 ] = [wx.NewId() for _init_ctrls in range(6)]
 
 class frJanela(wx.Frame):
-    def _init_coll_sbEstado_Fields(self, parent):
+    def _init_coll_stbEstado_Fields(self, parent):
         # generated method, don't edit
         parent.SetFieldsCount(1)
 
@@ -32,42 +32,41 @@ class frJanela(wx.Frame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wx.Frame.__init__(self, id=wxID_FRJANELA, name=u'frJanela', parent=prnt,
-              pos=wx.Point(323, 277), size=wx.Size(524, 379),
+              pos=wx.Point(448, 344), size=wx.Size(303, 327),
               style=wx.DEFAULT_FRAME_STYLE, title=u'Hand Talks!')
-        self.SetClientSize(wx.Size(516, 345))
+        self.SetClientSize(wx.Size(303, 327))
 
-        self.sbEstado = wx.StatusBar(id=wxID_FRJANELASBESTADO, name=u'sbEstado',
-              parent=self, style=0)
-        self._init_coll_sbEstado_Fields(self.sbEstado)
-        self.SetStatusBar(self.sbEstado)
+        self.stbEstado = wx.StatusBar(id=wxID_FRJANELASTBESTADO,
+              name=u'stbEstado', parent=self, style=0)
+        self._init_coll_stbEstado_Fields(self.stbEstado)
+        self.SetStatusBar(self.stbEstado)
 
-        self.stLetra = wx.StaticText(id=wxID_FRJANELASTLETRA, label=u'A',
-              name=u'stLetra', parent=self, pos=wx.Point(0, 0),
-              size=wx.Size(121, 190), style=0)
-        self.stLetra.SetFont(wx.Font(100, wx.SWISS, wx.NORMAL, wx.BOLD, False,
+        self.stLetra = wx.StaticText(id=wxID_FRJANELASTLETRA, label=u'',
+              name=u'stLetra', parent=self, pos=wx.Point(8, 16),
+              size=wx.Size(280, 225), style=0)
+        self.stLetra.SetFont(wx.Font(150, wx.SWISS, wx.NORMAL, wx.BOLD, False,
               u'Arial'))
-        self.stLetra.SetAutoLayout(False)
-        self.stLetra.SetThemeEnabled(False)
-        self.stLetra.SetWindowVariant(wx.WINDOW_VARIANT_LARGE)
 
         self.btSair = wx.Button(id=wxID_FRJANELABTSAIR, label=u'Sair',
-              name=u'btSair', parent=self, pos=wx.Point(152, 152),
-              size=wx.Size(64, 32), style=0)
+              name=u'btSair', parent=self, pos=wx.Point(8, 248),
+              size=wx.Size(80, 40), style=0)
         self.btSair.Bind(wx.EVT_BUTTON, self.OnSairButton,
               id=wxID_FRJANELABTSAIR)
 
         self.chLetras = wx.Choice(choices=[], id=wxID_FRJANELACHLETRAS,
-              name=u'chLetras', parent=self, pos=wx.Point(160, 24),
-              size=wx.Size(56, 21), style=0)
+              name=u'chLetras', parent=self, pos=wx.Point(112, 256),
+              size=wx.Size(80, 24), style=0)
+        self.chLetras.Bind(wx.EVT_CHOICE, self.OnChLetrasChoice,
+              id=wxID_FRJANELACHLETRAS)
 
         self.btTocar = wx.Button(id=wxID_FRJANELABTTOCAR, label=u'Tocar',
-              name=u'btTocar', parent=self, pos=wx.Point(160, 56),
-              size=wx.Size(56, 24), style=0)
+              name=u'btTocar', parent=self, pos=wx.Point(200, 248),
+              size=wx.Size(88, 40), style=0)
         self.btTocar.SetMinSize(wx.Size(80, 30))
 
     def __init__(self, parent):
         self._init_ctrls(parent)
-        self.chLetras.AppendItems(['A', 'B'])
+        self.chLetras.AppendItems( [chr (ord('A') + x) for x in range (26)] )
         self.chLetras.Select(0)
         ib = wx.IconBundle()
         ib.AddIconFromFile("handtalks.ico",wx.BITMAP_TYPE_ANY)
@@ -75,6 +74,9 @@ class frJanela(wx.Frame):
 
     def OnSairButton(self, event):
         # TODO: Confirmar saida
-
         self.Close()
+
+    def OnChLetrasChoice(self, event):
+        self.stLetra.SetLabel (self.chLetras.GetStringSelection())
+        event.Skip()
 
