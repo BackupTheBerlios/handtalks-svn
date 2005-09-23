36466aae7dd037ee7fc390944a965477132813f7
acdcp <acdcp@922b9c94-87f7-0310-a3a0-d3017a4e10ae>
2005-09-23 04:56:25 +0000

    Primeiro "deliverable"

---
+++ marcos/v0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+import py2exe
+import glob
+
+setup (
+    name = 'HandTalks!',
+    description = 'Tradutor do alfabeto LIBRAS',
+    version = '0.1', 
+    
+    windows = [{
+        "script": "handtalks.py",
+        "icon_resources": [(1, r"lib\handtalks.ico")]
+    }],
+    data_files = [ ("lib", glob.glob(r"lib\*")),
+                   ("audio", glob.glob(r"audio\*"))   ],
+    zipfile = r"lib\handtalks.lib",
+)
---
+++ marcos/v0.1/tocador.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+
+def toca_tudo (name, card=0):
+    for ext in ['ogg', 'mp3', 'wav']:
+        try:
+            toca (name + '.' + ext, card)
+        except IOError:
+            pass
+        except:
+            return False
+        else:
+            return True
+    return False
+
+def toca (name, card=0):
+    import pymedia.muxer as muxer
+    import pymedia.audio.acodec as acodec
+    import pymedia.audio.sound as sound
+    import time,wave
+    snds = sound.getODevices ()
+    if card not in range (len (snds)):
+        raise 'Cannot play sound to non existent device %d out of %d' % (card+1, len(snds))
+    ext = name.split('.')[-1].lower()
+    if ext == 'wav':
+        f = wave.open (name, 'rb')
+        snd = sound.Output (f.getframerate(), f.getnchannels(), sound.AFMT_S16_LE, card)
+        s= ' '
+        while len(s):
+            s = f.readframes (512)
+            snd.play (s)
+    else:
+        dm = muxer.Demuxer (ext)
+        f = open (name, 'rb')
+        snd = dec = None
+        s = f.read (32*1024)
+        t = 0
+        while len (s):
+            frames = dm.parse (s)
+            if frames:
+                for fr in frames:
+                    # Assume for now only audio streams
+                    if dec == None:
+                        dec = acodec.Decoder (dm.streams[fr[0]])
+              
+                    r = dec.decode (fr[1])
+                    if r and r.data:
+                        if snd == None:
+                            snd = sound.Output (r.sample_rate, r.channels, sound.AFMT_S16_LE, card)
+                        snd.play (r.data)
+              
+            s = f.read (512)
+
+    while snd.isPlaying():
+        time.sleep (.05)
+
+
+def main():
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print "Usage: aplayer <filename> [ sound_card_index ]"
+    else:
+        i= 0
+        if len(sys.argv) > 2 :
+            i = int (sys.argv[2])
+        if not toca_tudo (sys.argv[1], i):
+            print "Arquivo não encontrado!"
+
+if __name__ == '__main__':
+    main()
+
+# vim:sw=4
